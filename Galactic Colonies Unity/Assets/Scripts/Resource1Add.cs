3f2bf8bbc79b06308dd61fdc83ec94ba252ee30b
Unknown <ewawoowaa@gmail.com>
2019-11-18 17:00:45 +0000

    First Upload

---
+++ Galactic Colonies Unity/Assets/Scripts/Resource1Add.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource1Add : MonoBehaviour
+{
+    public GameObject GameMaster;
+    private gameMaster gMScript;
+
+    void awake()
+    {
+        if (GameMaster != null)
+        {
+            gMScript = GameMaster.GetComponent<gameMaster>();
+        }
+        else
+        {
+            print("No Object");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        gMScript = GameMaster.GetComponent<gameMaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gMScript != null)
+        {
+            gMScript.resource1 = gMScript.resource1 + 1;
+        }
+        else
+        {
+            print("No GM");
+            //gMScript = GameMaster.GetComponent<gameMaster>();
+        }
+    }
+}
---
+++ Galactic Colonies Unity/Assets/Scripts/gameMaster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameMaster : MonoBehaviour
+{
+    public int resource1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        resource1 = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // resource1 = resource1 + 1;
+        print (resource1);
+    }
+}
